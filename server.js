3ddf174d2e3106ca4f5ad3f16c40095a0c2ce14b
HAFIS-DAVIES <hafis.davies01@gmail.com>
2025-07-10 22:27:57 -0600

    vunerability-resolved

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-//example vulnerable code
-mockSocket = new MockSocket()
+// //example vulnerable code
+// mockSocket = new MockSocket()
